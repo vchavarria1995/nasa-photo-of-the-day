36f96820c793356f369abfa26bfcaee31b7bb1b4
vchavarria1995 <vchavarria1995@gmail.com>
2021-07-14 20:48:14 -0500

    initial code, api set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import Image from './components/image';
+
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod?';
+export const API_KEY = 'api_key=0AKd7aYfJUgofrr7rF5RuQLOb2ALjPEWkzYFgiKo'
 
 function App() {
+  const [imgData, setImgData] =useState([]);
+
+  useEffect(() => {
+    const getImage = () =>{
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=0AKd7aYfJUgofrr7rF5RuQLOb2ALjPEWkzYFgiKo&date=2012-03-14')
+      .then(res => {
+        setImgData(res.data);
+      })
+      .catch((err) => console.log('Error: ' + err));
+    };
+    getImage();
+    }, []);
+
   return (
     <div className="App">
       <p>
