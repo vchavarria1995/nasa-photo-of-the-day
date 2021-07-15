22acccc96e765bd1591c9bb176d3779dd6d17cc4
vchavarria1995 <vchavarria1995@gmail.com>
2021-07-14 21:03:37 -0500

    added place holder text for now, working on components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from "axios";
-import Image from './components/image';
+import Placeholder from './components/placeholder';
 
 export const BASE_URL = 'https://api.nasa.gov/planetary/apod?';
 export const API_KEY = 'api_key=0AKd7aYfJUgofrr7rF5RuQLOb2ALjPEWkzYFgiKo'
@@ -23,9 +23,14 @@ function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        NASA Picture of the Day! <span role="img" aria-label='go!'>🚀</span>
       </p>
+      <p>{imgData.title}</p>
+      <p>{imgData.date}</p>
+      <p>{imgData.explanation}</p>
+      <p>{imgData.hdurl}</p>
+      <img src={imgData.url} alt="NASA pic of the day"/>
+      <p>{imgData.copyright}</p>
     </div>
   );
 }
